7cec706ee98b81b9aa937fd8936638eccfba760f
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-03-25 23:01:57 -0400

    rough draft complete, works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 
 // Main variables
 var events = []
-var 
-var
+
 // current date 
-var today = moment().format("mm, dd, yyyy")
-console.log(today)
+var today = moment().format("MMM Do YYYY")
+//console.log(today)
 $("#currentDay").text(today);
 // local storage functions
 var getEvents = () => {
@@ -17,13 +16,13 @@ var getEvents = () => {
     for (let i = 0; i < events.length; i++) {
         var loadSection = $(".hour:contains('" + events[i].time + "')")
        // console.log(loadSection);
-        loadSection.siblings(".textSection").children(".eventInfo").text(events[i].text)
+        loadSection.siblings(".textSection").children(".eventPInfo").text(events[i].text)
     }
 
 
     $('.hour').each(function() {
-        checkevents(this)
-        //console.log(checkEvents)
+        checkEvents(this)
+       // console.log(checkEvents)
     });
 }
 // function for saving events to the local storage
@@ -33,21 +32,87 @@ localStorage.setItem("events", JSON.stringify(events));
 
 // create p element that can be editied into textArea
 $(".timeSection").on("click", ".textSection", function() {
-    let getText = $(this).text().trim()
-    //console.log(text)
-    var input = $("<textArea>").addClass("kkk").val(text)
+    var text = $(this).text().trim()
+    console.log(this)
+
+    var textAdd = $("<textArea>").addClass("boxy").val(text)
     var changeP = $("p", this);
-    changeP.replaceWith(getText)
-    getText.trigger("focus")
+    changeP.replaceWith(textAdd)
+    textAdd.trigger("focus")
 //check if input is past present or future and add styling
     if ($(this).hasClass("past")) {
-        textInput.addClass("pastTextArea")
+        textAdd.addClass("pastTextArea")
     }
     else if ($(this).hasClass("present")) {
-        textInput.addClass("presentTextArea")
+        textAdd.addClass("presentTextArea")
     }
     else if ($(this).hasClass("future")) {
-        textInput.addClass("futureTextArea")
+        textAdd.addClass("futureTextArea")
     }
 });
+//save button
+$(".saveButton").on("click", function() {
+    var eventInfo = $(this).siblings(".textSection").children("textarea").val();
+   
+    if(eventInfo === undefined) {
+        return;
+    }
+    var eventTime = $(this).siblings(".hour").text();
+    var newInfo = true;
+    
+    for (var i = 0; i < events.length; i++) {   
+        if (events[i].time === eventTime) {  
+            newInfo = false;
+            events[i].text = eventInfo;
 
+            if (eventInfo === ""){
+                events.splice(i, 1);
+                break;
+            }
+        }
+    }
+    
+    if (newInfo === true) {
+        events.push({time: eventTime, text: eventInfo});
+    }
+    
+    var changeP = $("<p>").addClass("eventPInfo").text(eventInfo);
+    $(this).siblings(".textSection").children("textarea").replaceWith(changeP);
+
+    saveEvents()
+});
+
+checkEvents = (eventElement) => {
+
+    var sectionTime = $(eventElement).text();
+    sectionTime = sectionTime.split(" ");
+   
+    if (sectionTime[1] === "pm" && sectionTime[0] !== "12"){
+        sectionTime[0] = parseInt(sectionTime[0]);
+       
+        sectionTime[0] += 12;
+    }
+
+    var colorBox = $(eventElement).siblings(".textSection");
+    colorBox.removeClass("past present future");
+    if (moment().isAfter(moment().hour(sectionTime[0]))) {
+       // console.log("past");
+       colorBox.addClass("past");
+    } else if (moment().isSame(moment().hour(sectionTime[0]))) {
+        //console.log("present");
+        colorBox.addClass("present");
+    }   else if (moment().isBefore(moment().hour(sectionTime[0]))) {
+        //console.log(" future");
+        colorBox.addClass("future");
+    }
+};
+
+getEvents();
+
+//set timeClock to check events 
+var timeClock = setInterval(function() {
+    $(".hour").each(function() {
+        checkEvents(this);
+        //clearInterval(timer);
+    })
+}, 300000);
