3db9d1339ec8079a1c6138271b2b499b9d378371
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-03-25 20:52:42 -0400

    added more savetask js and textarea function

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,28 @@ var getEvents = () => {
         //console.log(checkEvents)
     });
 }
+// function for saving events to the local storage
+var saveEvents = () => {
+localStorage.setItem("events", JSON.stringify(events));
+}
+
+// create p element that can be editied into textArea
+$(".timeSection").on("click", ".textSection", function() {
+    let getText = $(this).text().trim()
+    //console.log(text)
+    var input = $("<textArea>").addClass("kkk").val(text)
+    var changeP = $("p", this);
+    changeP.replaceWith(getText)
+    getText.trigger("focus")
+//check if input is past present or future and add styling
+    if ($(this).hasClass("past")) {
+        textInput.addClass("pastTextArea")
+    }
+    else if ($(this).hasClass("present")) {
+        textInput.addClass("presentTextArea")
+    }
+    else if ($(this).hasClass("future")) {
+        textInput.addClass("futureTextArea")
+    }
+});
+
